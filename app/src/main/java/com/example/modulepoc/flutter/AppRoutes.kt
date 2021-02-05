9697700c867ccddcd221855bcba75683112b36c7
satish <xsatishtamarkar@moneytap.com>
2021-02-05 15:00:53 +0530

    First commit

---
+++ app/src/main/java/com/example/modulepoc/flutter/AppRoutes.kt
@@ -0,0 +1,11 @@
+package com.example.modulepoc.flutter
+
+class AppRoutes {
+    companion object {
+        @JvmStatic
+        var home = "/home";
+
+        @JvmStatic
+        var screen1 = "/screen1";
+    }
+}
---
+++ app/src/main/java/com/example/modulepoc/flutter/Flutter.kt
@@ -0,0 +1,12 @@
+package com.example.modulepoc.flutter
+
+class Flutter{
+
+companion object{
+    @JvmStatic
+    fun initWithRoute(route:String){
+
+    }
+}
+
+}
